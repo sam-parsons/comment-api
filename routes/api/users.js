4baa8c03e9665b3a3fc010497a435b69d7c249a6
Sam Parsons <samparsons269@gmail.com>
2019-03-27 13:54:02 -0400

    here we go again

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 // const keys = require("../../config/keys");
 const passport = require("passport");
