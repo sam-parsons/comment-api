8dc98c99c4fefb04169c48190f3fd753a2dc8ef0
Sam Parsons <samparsons269@gmail.com>
2019-03-27 13:21:09 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+// const profile = require("./routes/api/profile");
+// const posts = require("./routes/api/posts");
+
+const app = express();
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//use routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => console.log(`server running on ${port}`));
